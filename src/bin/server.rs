bef5400b7aa5ff90cca0d4ac41d814bedf00199b
Tinashe Mundangepfupfu <tinashe.mundangepfupfu@deliveryhero.com>
2022-11-20 14:54:34 +0100

    NO_REF:Read supplied command from terminal

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,6 +1,8 @@
 use tokio::net::TcpListener;
 use bytes::BytesMut;
 use tokio::io::AsyncReadExt;
+use nomad_redis::Command;
+use nomad_redis::helper::buffer_to_array;
 
 // transform async main to a sync function
 #[tokio::main]
@@ -13,7 +15,9 @@ pub async fn main() -> Result<(), std::io::Error> {
         let mut buf = BytesMut::with_capacity(1024);
         socket.read_buf(&mut buf).await?;
         println!("buffer {:?}", buf); // printing the data in the buffer
-    }
 
+        let attrs = buffer_to_array(&mut buf);
+        let command = Command::get_command(&attrs[0]);
+    }
     Ok(())
 }
---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+pub mod helper;
+pub mod cmd;
+pub use cmd::Command;
